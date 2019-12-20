f954816f080298abaf58980e0841062c0a794083
Gabriel <olaniyi_g@hotmail.com>
2019-12-20 06:35:06 +0100

    config key is checked and reset into .env

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,20 +2,12 @@ import app from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  // apiKey: process.env.REACT_APP_API_KEY,
-  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_DATABASE_URL,
-  // projectId: process.env.REACT_APP_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
-  apiKey: "AIzaSyAwLr8FChNiZUE3nfn_GOmEfkR85McPOSA",
-  authDomain: "react-authentication-225c5.firebaseapp.com",
-  databaseURL: "https://react-authentication-225c5.firebaseio.com",
-  projectId: "react-authentication-225c5",
-  storageBucket: "react-authentication-225c5.appspot.com",
-  messagingSenderId: "1059171840276",
-  appId: "1:1059171840276:web:c9779578a7a89864ff1201",
-  measurementId: "G-FVCSSLKM2Z"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 };
 
 class Firebase {
